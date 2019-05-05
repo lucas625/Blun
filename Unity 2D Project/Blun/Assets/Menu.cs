6f5902496c96d05b7ba8ca194ecb5f69c1c1cd21
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2019-05-05 17:19:58 -0300

    Menu de Start implementado

---
+++ Unity 2D Project/Blun/Assets/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    //Inicializacao dos butoes
+    public Button startButton;
+    public Button personalizarButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PersonalizarButtonPress()
+    {
+       
+    }
+}
