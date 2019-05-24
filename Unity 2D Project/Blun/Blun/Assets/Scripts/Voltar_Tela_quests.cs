d7e383218453b18fe2eceac65df11f8dd9291e06
Igor <iast@cin.ufpe.br>
2019-05-24 17:10:16 -0300

    tela game 1 pronta

---
+++ Unity 2D Project/Blun/Blun/Assets/Scripts/Voltar_Tela_quests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Voltar_Tela_quests : MonoBehaviour {
+	//Inicializacao dos butoes
+	//Nome é irrelevante
+    	public Button Voltar_Tela_questsButton;
+    	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Voltar_Tela_questsButtonPress()
+    	{
+        	SceneManager.LoadScene(1);
+    	}
+
+}
