23f80f0641dee0becb3c9aaa8a1e04ee1b1fced1
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2019-05-24 15:07:50 -0300

    Scripts implementados e adicionados aos botões

--- Unity 2D Project/Blun/Blun/Assets/Scripts/StartMenu.cs
+++ Unity 2D Project/Blun/Blun/Assets/Scripts/StartMenu.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour {
-
+	//Inicializacao dos butoes
+	//Nome é irrelevante
+    public Button LoginButton;
+    public Button CadastroButton;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +18,14 @@ public class StartMenu : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public void LoginButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CadastroButtonPress()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
