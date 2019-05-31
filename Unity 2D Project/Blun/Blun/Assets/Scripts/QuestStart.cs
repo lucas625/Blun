cf07f9f1d76b666f96d83e5488ca99fdf01a6c70
Italo Soares <irs@cin.ufpe.br>
2019-05-31 16:55:53 -0300

    Correções de PR

---
+++ Unity 2D Project/Blun/Blun/Assets/Scripts/QuestStart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuestStart : MonoBehaviour {
+	//Inicializacao dos botoes
+	//Nome é irrelevante
+    public Button HomeButton;
+    public Button AjudaButton;
+    //public Button TopLeftButton;
+    //public Button TopRightButton;
+    public string HomeLevel= "HomeScene";
+    public string AjudaLevel= "DumbScene";
+    public string TopLeftButton= "DumbScene";
+    public string TopRightButton= "DumbScene";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void HomeButtonPress()
+    {
+        SceneManager.LoadScene(HomeLevel);
+    }
+
+    public void AjudaButtonPress()
+    {
+        SceneManager.LoadScene(AjudaLevel);
+    }
+
+    public void TopLeftButtonPress()
+    {
+        SceneManager.LoadScene(TopLeftButton);
+    }
+
+    public void TopRightButtonPress()
+    {
+        SceneManager.LoadScene(TopRightButton);
+
+    }
+}
