8f357b16e1e3fccec1f5930bbf6f621dbc1afedd
Humberto Alves Wanderley Neto <hawn@cin.ufpe.br>
2019-06-07 16:56:09 -0300

    tela resultados v1 concluída

---
+++ Unity 2D Project/Blun/Blun/Assets/Scripts/Pontuacao.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour
+{
+    public Button finishButton;
+    public Text labelCongrats;
+    public Text scoreValue;
+    // Use this for initialization
+    void Start()
+    {
+        labelCongrats.text = "Parabéns " + PlayerPrefs.GetString("User") + "! Você concluíu a aventura com sucesso.";
+        scoreValue.text = PlayerPrefs.GetInt("score").ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void finishButtonPress()
+    {
+        SceneManager.LoadScene("QuestStart");
+    }
+
+}
